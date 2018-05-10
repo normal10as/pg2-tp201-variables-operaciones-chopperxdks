031819b11cfdab359871b43d74fb7c75c6cbe183
Fernando.S <chopperxdks@gmail.com>
2018-05-09 22:32:21 -0300

    A

--- Variables_operaciones/Variables_operaciones/1_16.vb
+++ Variables_operaciones/Variables_operaciones/1_16.vb
@@ -2,7 +2,7 @@
     Sub main()
         Dim dias As Byte
         Dim fecha As Date
-        Console.WriteLine("ingrese una fecha: ")
+        Console.WriteLine("ingrese una fecha(dd/mm/aaaa): ")
         fecha = Console.ReadLine
         Console.WriteLine("Ingrese dias que quieras sumar: ")
         dias = Console.ReadLine
